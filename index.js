ffcd4951525e1e92a77dd484312850083faa3365
nightfall <68837055+rolyquellca2020@users.noreply.github.com>
2021-07-03 21:05:54 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function sum( a, b, c ) {
+    let numberA = a, numberB = b, numberC = c;
+    let suma = numberA + numberB + numberC;
+    const evento  = document.getElementById("send");
+    evento.addEventListener('onclick', function mode(req, res) {
+        res.send("hola");
+        res.send("asdm");
+    })
+    
+    return  suma;
+}
+const val = sum(1,5,3);
+console.log(val);
